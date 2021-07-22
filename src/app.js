994e9c811fd9b267996ad277031ee2d66bc6a685
Aimecita1 <87346780+Aimecita1@users.noreply.github.com>
2021-07-22 14:07:48 -0400

    Added new CSS style for units and removed forecast

--- src/app.js
+++ src/app.js
@@ -62,17 +62,4 @@ function showTemperature(response) {
 let searchCity = document.querySelector("#city-form");
 searchCity.addEventListener("submit", isCity);
 
-function showPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiKey = "98980a45697353d2d771a3c81708573c";
-  let weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
-  axios.get(`${weatherURL}&appid=${apiKey}`).then(showTemperature);
-}
-
-function getCurrentPosition() {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-
 search("Ponce");
