c3abab48cda3987c91b0d2422512a9d1470896c0
Aimecita1 <87346780+Aimecita1@users.noreply.github.com>
2021-07-25 17:14:43 -0400

    Update app.js with API URL for daily forecast

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hour}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -47,6 +48,13 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let units = "metric";
+  let apiKey = "98980a45697353d2d771a3c81708573c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function search(city) {
@@ -86,6 +94,8 @@ function showTemperature(response) {
     .querySelector("#icon")
     .setAttribute("alt", response.data.weather[0].description);
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function displayFahrenheitTemperature(event) {
@@ -118,5 +128,3 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusTemperature = null;
 
 search("Ponce");
-
-displayForecast();
